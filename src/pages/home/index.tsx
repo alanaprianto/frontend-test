c79632d9ea1330d3c56c4e064de707f3eeeef83f
alan aprianto <alan.aprianto@gmail.com>
2021-06-20 18:22:41 +0700

    feat: list planet

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,79 @@
+import { useState, useEffect } from "react";
+import {
+  Container,
+  ListComponent,
+  ItemList,
+  LoadingData,
+  H1Title,
+} from "./style";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useHistory } from "react-router-dom";
+
+type ResponseType = {
+  count: number;
+  next: string;
+  previous: string;
+  results: Array<object>;
+};
+
+function Home() {
+  const history = useHistory();
+  const [page, setPage] = useState(1);
+  const [response, setResponse] = useState<ResponseType>({
+    count: 0,
+    next: "",
+    previous: "",
+    results: [],
+  });
+
+  const fetchData = async () => {
+    const params = new URLSearchParams({
+      page: page.toString(),
+    });
+    const res = await fetch(`https://swapi.dev/api/planets?${params}`);
+    const data = await res.json();
+    setResponse({
+      ...response,
+      ...data,
+      results: response.results.concat(data.results),
+    });
+  };
+
+  const fetchNext = () => {
+    if (response.results.length < response.count) {
+      setTimeout(() => {
+        setPage(page + 1);
+      }, 500);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [page]);
+
+  return (
+    <Container>
+      <ListComponent>
+        <H1Title>Planet List</H1Title>
+        <hr />
+        <InfiniteScroll
+          dataLength={response.results.length}
+          next={fetchNext}
+          hasMore={response.results.length < response.count}
+          loader={<LoadingData>Loading...</LoadingData>}
+        >
+          {response.results.map((i: { [key: string]: any }, index) => (
+            <ItemList
+              key={index}
+              onClick={() => history.push(`/detail/${index + 1}`)} // swapi not return id, use index + 1 instead
+            >
+              {index + 1}) Planet - {i.name}
+            </ItemList>
+          ))}
+        </InfiniteScroll>
+      </ListComponent>
+    </Container>
+  );
+}
+
+export default Home;
---
+++ src/pages/home/style.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+
+export const Container = styled.div`
+display: flex;
+flex-direction: row;
+align-items: center;
+justify-content: center;
+`;
+
+export const ListComponent = styled.div`
+border: 1px solid green;
+width: 500px;
+padding: 1rem;
+`
+
+export const ItemList = styled.div`
+height: 50px;
+border: 1px solid green;
+margin: 6px;
+padding: 8px;
+cursor: pointer;
+display: flex;
+flex-direction: row;
+align-items: center;
+`
+
+export const LoadingData = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+`
+
+export const H1Title = styled.h1`
+font-size: 20px
+`
